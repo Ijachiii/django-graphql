f719c90f07a0e8e99aaec07bc2e8739933e04481
David Audu <davidaudu1010@gmail.com>
2023-10-14 16:47:54 +0100

    crud operation on restaurant model

--- my_app/models.py
+++ my_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    addresses = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
